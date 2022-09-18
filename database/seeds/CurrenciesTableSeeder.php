057225751b6938a1d161647c6ed67338ae974bb6
kasunvimarshana <kasunvmail@gmail.com>
2022-09-18 23:15:37 +0530

    create models, migrations, seeds

---
+++ database/seeds/CurrenciesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Currency;
+
+class CurrenciesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $currencies = [
+            'usd',
+            'eur',
+            'gbp',
+            'jpy',
+        ];
+
+        foreach ($currencies as $currency) {
+            Currency::create([
+                'iso' => $currency,
+            ]);
+        }
+    }
+}
