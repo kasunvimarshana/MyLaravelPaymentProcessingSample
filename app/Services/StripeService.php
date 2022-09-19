17257d1a6760dbb4e54ef7630f30ad8e9d98746f
kasunvimarshana <kasunvmail@gmail.com>
2022-09-19 17:07:53 +0530

    edit stripe service

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -15,11 +15,14 @@ class StripeService
 
     protected $secret;
 
+    protected $plans;
+
     public function __construct()
     {
         $this->baseUri = config('services.stripe.base_uri');
         $this->key = config('services.stripe.key');
         $this->secret = config('services.stripe.secret');
+        $this->plans = config('services.stripe.plans');
     }
 
     public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
@@ -113,5 +116,93 @@ class StripeService
             'POST',
             "/v1/payment_intents/{$paymentIntentId}/confirm",
         );
+    }
+
+    public function handleSubscription(Request $request)
+    {
+        $customer = $this->createCustomer(
+            $request->user()->name,
+            $request->user()->email,
+            $request->payment_method,
+        );
+
+        $subscription = $this->createSubscription(
+            $customer->id,
+            $request->payment_method,
+            $this->plans[$request->plan],
+        );
+
+        if ($subscription->status == 'active') {
+            session()->put('subscriptionId', $subscription->id);
+
+            return redirect()->route('subscribe.approval', [
+                'plan' => $request->plan,
+                'subscription_id' => $subscription->id,
+            ]);
+        }
+
+        $paymentIntent = $subscription->latest_invoice->payment_intent;
+
+
+        if ($paymentIntent->status === 'requires_action') {
+            $clientSecret = $paymentIntent->client_secret;
+
+            session()->put('subscriptionId', $subscription->id);
+
+            return view('stripe.3d-secure-subscription')->with([
+                'clientSecret' => $clientSecret,
+                'plan' => $request->plan,
+                'paymentMethod' => $request->payment_method,
+                'subscriptionId' => $subscription->id,
+            ]);
+        }
+
+        return redirect()
+            ->route('subscribe.show')
+            ->withErrors('We were unable to activate the subscrption. Try again, please.');
+    }
+
+    public function createCustomer($name, $email, $paymentMethod)
+    {
+        return $this->makeRequest(
+            'POST',
+            '/v1/customers',
+            [],
+            [
+                'name' => $name,
+                'email' => $email,
+                'payment_method' => $paymentMethod,
+            ],
+        );
+    }
+
+    public function createSubscription($customerId, $paymentMethod, $priceId)
+    {
+        return $this->makeRequest(
+            'POST',
+            '/v1/subscriptions',
+            [],
+            [
+                'customer' => $customerId,
+                'items' => [
+                    ['price' => $priceId],
+                ],
+                'default_payment_method' => $paymentMethod,
+                'expand' => ['latest_invoice.payment_intent'],
+            ],
+        );
+    }
+
+    public function validateSubscription(Request $request)
+    {
+        if (session()->has('subscriptionId')) {
+            $subscriptionId = session()->get('subscriptionId');
+
+            session()->forget('subscriptionId');
+
+            return $request->subscription_id == $subscriptionId;
+        }
+
+        return false;
     }
 }
